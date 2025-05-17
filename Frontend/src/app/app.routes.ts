9251cbb71767a3ec1f581d6552552d3998a74946
youssfmadani <youssfelmadani@gmail.com>
2025-05-17 13:57:05 +0100

    Ajout de comfiguration Budget list et form pour CRUD

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { BudgetListComponent } from './components/budget-list/budget-list.component';
+import { BudgetFormComponent } from './components/budget-form/budget-form.component';
+import { TransactionListComponent } from './components/transaction-list/transaction-list.component';
+import { TransactionFormComponent } from './components/transaction-form/transaction-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/budgets', pathMatch: 'full' },
+
+
+  { path: 'budgets', component: BudgetListComponent },
+  { path: 'budgets/add', component: BudgetFormComponent },
+  { path: 'budgets/edit/:id', component: BudgetFormComponent },
+
+
+  { path: 'transactions', component: TransactionListComponent },
+  { path: 'transactions/add', component: TransactionFormComponent },
+  { path: 'transactions/edit/:id', component: TransactionFormComponent },
+
+
+  { path: '**', redirectTo: '/budgets' }
+];
--- Frontend/src/app/components/budget/budget-list/budget-list.component.ts
+++ Frontend/src/app/components/budget/budget-list/budget-list.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BudgetService } from '../../../services/budget.service';
+import { Budget } from '../../../models/budget.model';
+import {BudgetFormComponent} from '../budget-form/budget-form.component';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-budget-list',
-  imports: [],
   templateUrl: './budget-list.component.html',
-  styleUrl: './budget-list.component.css'
+  imports: [
+    BudgetFormComponent,CommonModule,FormsModule
+  ],
+  styleUrls: ['./budget-list.component.css']
 })
-export class BudgetListComponent {
+export class BudgetListComponent implements OnInit {
+  budgets: Budget[] = [];
+  selectedBudget?: Budget;
 
+  constructor(private budgetService: BudgetService) {}
+
+  ngOnInit(): void {
+    this.loadBudgets();
+  }
+
+  loadBudgets(): void {
+    this.budgetService.getAllBudgets().subscribe((data: Budget[]) => this.budgets = data);
+  }
+
+  deleteBudget(id?: number): void {
+    if (id) {
+      this.budgetService.deleteBudget(id).subscribe(() => this.loadBudgets());
+    }
+  }
+
+  selectBudget(budget: Budget): void {
+    this.selectedBudget = { ...budget };  // clone to edit
+  }
+
+  clearSelection(): void {
+    this.selectedBudget = undefined;
+  }
 }
